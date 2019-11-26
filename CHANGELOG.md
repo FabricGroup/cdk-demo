692eb950ad546e2b81a6eef55cb1255e6517aae4
Pk <pk.srivastava@auspost.com.au>
2019-11-26 20:49:48 +1100

    Adding deployer role in infrastructure stack

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+#Changelog
+
+##Checkpoint 1
+Create Infra stack with deployer role.
+Deployer role will be used to deploy cloudformation stacks.
--- test/cdk.test.ts
+++ test/cdk.test.ts
@@ -1,13 +1,15 @@
-import { expect as expectCDK, matchTemplate, MatchStyle } from '@aws-cdk/assert';
-import cdk = require('@aws-cdk/core');
-import Cdk = require('../lib/cdk-stack');
+import { expect as expectCDK, haveResourceLike, ResourcePart } from '@aws-cdk/assert'
+import { InfraStack } from '../lib/infra-stack'
+import cdk = require('@aws-cdk/core')
 
-test('Empty Stack', () => {
-    const app = new cdk.App();
+test('Should create infra stack', () => {
+    const app = new cdk.App()
     // WHEN
-    const stack = new Cdk.CdkStack(app, 'MyTestStack');
+    const stack = new InfraStack(app, 'InfraStack')
     // THEN
-    expectCDK(stack).to(matchTemplate({
-      "Resources": {}
-    }, MatchStyle.EXACT))
-});
+    expectCDK(stack).to(haveResourceLike('AWS::IAM::Role', {
+        Properties: {
+            RoleName: 'cdk-deployer'
+        }
+    }, ResourcePart.CompleteDefinition))
+})
